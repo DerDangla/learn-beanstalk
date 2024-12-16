52e1bc5d9c47ceefa619c49d005be8566ba4c6ca
Emander Dangla <emanderdangla@Emanders-Laptop.local>
2024-12-16 10:30:41 -0500

    Intial Commit

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for root URL
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Route for version endpoint
 @app.route('/version')
 def version():
-    return jsonify(version="1.0.0")
+    return "v1.0.0"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
